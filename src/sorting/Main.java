4a801f93283f3698d4dcd62e26b459665d2e8458
Denis Stoyanouski <vandenst@tut.by>
2023-02-04 17:02:56 +0300

    add some functions

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -1,14 +1,23 @@
 package sorting;
 
+import java.net.CookieHandler;
 import java.util.*;
 
 public class Main {
+
+    static List<Long> numbers = new ArrayList<>();
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            numbers.add(number);
         }
+        int number = numbers.size();
+        long maxValue = Collections.max(numbers);
+        int count = (int) numbers.stream().filter(x -> x == maxValue).count();
+
+        System.out.printf("Total numbers: %d%n", number);
+        System.out.printf("The greatest number: %d (%d time(s)).", maxValue, count);
     }
 }
